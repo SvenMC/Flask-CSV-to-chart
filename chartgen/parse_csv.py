1f27a6cb58c5893141610149e5a25dd79314b102
SvenMC <svencooke7@gmail.com>
2023-08-19 18:17:22 +0100

    clean csv file and build df

--- chartgen/parse_csv.py
+++ chartgen/parse_csv.py
@@ -7,3 +7,8 @@ class ParseCSV:
         file = pd.read_csv(r'playground\nvchart.csv')
         df = pd.DataFrame(file)
 
+        # Set first column as index
+        support_df = df.set_index(df.iloc[:, 0])
+
+        # Strip non numerical columns
+        self.support_df = support_df.select_dtypes('number')
